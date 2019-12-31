d10a9f154552668898392e4b7cebf1807976d2d8
John Haugeland <stonecypher@gmail.com>
2019-12-30 16:49:16 -0800

    running build, transfers

---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+
+import nodeResolve from 'rollup-plugin-node-resolve';
+import typescript  from 'rollup-plugin-typescript2';
+import commonjs    from 'rollup-plugin-commonjs';
+import ignore      from 'rollup-plugin-ignore';
+import json        from 'rollup-plugin-json';
+
+const pkg = require('./package');
+
+
+
+
+
+const cjsconfig = {
+
+  input     : 'src/ts/cli.ts',
+
+  output    : {
+    file      : 'build/cli.js',
+    format    : 'cjs',
+    name      : 'bin',
+    banner    : '#!/usr/bin/env node\n'
+  },
+
+  plugins   : [
+
+    json(),
+
+    ignore(['fs', 'path', 'crypto']),
+
+    typescript(),
+
+    commonjs({
+      include: 'node_modules/**'
+    }),
+
+    nodeResolve({
+      mainFields     : [ 'module', 'main' ],
+      browser        : true,
+      extensions     : [ '.js', '.json', '.ts' ],
+      preferBuiltins : false
+    })
+
+  ]
+
+};
+
+
+
+
+
+
+export default cjsconfig;
---
+++ src/ts/cli.ts
@@ -0,0 +1,58 @@
+
+// shebang is in rollup banner; see bin-rollup.config.js
+
+
+
+
+
+const app = require('commander');
+
+import { version }           from '../../package.json';
+import { fsl_to_svg_string } from 'jssm-viz';
+
+
+
+
+
+app
+  .version(version)
+  .option('-s, --source <glob>',      'The input source file, as a glob, such as foo.fsl or ./**/*.fsl')
+  .option('--svg <default>',          'Produce output in SVG format (default if no formats specified)')
+  .option('--png',                    'Produce output in PNG format')
+  .option('--jpg',                    'Produce output in JPEG format, with a .jpg extension')
+  .option('--jpeg',                   'Produce output in JPEG format, with a .jpeg extension')
+  .option('--gif',                    'Produce output in GIF format')
+  .option('--webp',                   'Produce output in WEBP format')
+  .option('-w, --width <integer>',    'Set raster render width, in pixels')
+  .option('--tree',                   'Produce output in JSSM\'s internal parse tree format, with a .tree extension')
+  .option('--dot',                    'Produce output in GraphViz\'s DOT format')
+  .option('--inplace <default>',      'Output where source was found')
+  .option('--todir <dir>',            'Output to a specified directory')
+  .option('--toinplacedir <dir>',     'Output a matching tree from source to a specified directory')
+  .option('--tosourcenameddir <dir>', 'Output slugged names to a specified directory');
+
+
+
+
+
+async function render(fsl_code: string): Promise<string> {
+
+  const svg_code: string = await fsl_to_svg_string(fsl_code);
+
+  return svg_code;
+
+}
+
+
+
+
+
+function run() {
+  console.log('it ran');
+}
+
+
+
+
+
+run();
