4bb51460514acf5a457022aaf9c40e663d168073
John Haugeland <stonecypher@gmail.com>
2020-01-16 21:15:32 -0800

    functional cli behavior

--- src/ts/cli.ts
+++ src/ts/cli.ts
@@ -5,7 +5,10 @@
 
 
 
-const app = require('commander');
+const fs   = require('fs');
+
+const app  = require('commander'),
+      glob = require('glob');
 
 import { version }           from '../../package.json';
 import { fsl_to_svg_string } from 'jssm-viz';
@@ -17,21 +20,33 @@ import { file_type }         from './types';
 
 
 app
+
   .version(version)
+
   .option('-s, --source <glob>',      'The input source file, as a glob, such as foo.fsl or ./**/*.fsl')
-  .option('--svg <default>',          'Produce output in SVG format (default if no formats specified)')
+
+  .option('-w, --width <integer>',    'Set raster render width, in pixels')
+
+  .option('-v, --verbose',            'Log to console normally')
+  .option('-q, --quiet',              'Only log to console on error')
+  .option('-z, --silent',             'Do not log to console at all')
+
+  .option('--svg',                    'Produce output in SVG format (default if no formats specified)')
   .option('--png',                    'Produce output in PNG format')
   .option('--jpg',                    'Produce output in JPEG format, with a .jpg extension')
   .option('--jpeg',                   'Produce output in JPEG format, with a .jpeg extension')
   .option('--gif',                    'Produce output in GIF format')
   .option('--webp',                   'Produce output in WEBP format')
-  .option('-w, --width <integer>',    'Set raster render width, in pixels')
   .option('--tree',                   'Produce output in JSSM\'s internal parse tree format, with a .tree extension')
   .option('--dot',                    'Produce output in GraphViz\'s DOT format')
-  .option('--inplace <default>',      'Output where source was found')
+
+  .option('--inplace',                'Output where source was found (default)')
   .option('--todir <dir>',            'Output to a specified directory')
   .option('--toinplacedir <dir>',     'Output a matching tree from source to a specified directory')
-  .option('--tosourcenameddir <dir>', 'Output slugged names to a specified directory');
+  .option('--tosourcenameddir <dir>', 'Output slugged names to a specified directory')
+  .option('--topipe',                 'Output to pipe 0 (aka cout, stdout)');
+
+app.parse(process.argv);
 
 
 
@@ -49,15 +64,80 @@ async function render(fsl_code: string): Promise<string> {
 
 
 
+function english_list(list) {
+
+  if (list.length === 0) { return ""; }
+  if (list.length === 1) { return `${list[0]}`; }
+  if (list.length === 2) { return `${list[0]} and ${list[1]}`; }
+
+  const [ last, ... r_front ] = list.reverse(),
+        front                 = r_front.reverse();
+
+  return `${front.join(', ')}, and ${last}`;
+
+}
+
+
+
+
+
+function validate_args() {
+
+  if (!(app.source)) {
+    console.log('Error: must specify a source file or source glob');
+    process.exit(1);
+  }
+
+  const dirs   = ['inplace', 'todir', 'toinplacedir', 'tosourcenameddir', 'topipe'],
+        dirsOn = dirs.filter(d => app[d] !== undefined);
+
+  if (dirsOn.length > 1) {
+    console.log(`${english_list(dirsOn)} are mutually exclusive.  Please choose at most one.`);
+    process.exit(1);
+  }
+
+  const noise = ['verbose', 'quiet', 'silent'];
+  if (noise.filter(d => app[d] !== undefined).length > 1) {
+    console.log(`${english_list(noise)} are mutually exclusive.  Please choose at most one.`);
+    process.exit(1);
+  }
+
+  if (dirsOn.length === 0) {
+    app.inplace = true;
+  }
+
+  const imgFormats = ['png', 'svg', 'jpg', 'jpeg', 'gif', 'webp', 'tree', 'dot'];
+  if (imgFormats.filter(d => app[d] !== undefined).length === 0) {
+    app.svg = true;
+  }
+
+}
+
+
+
+
+
+async function output({ fname, data }) {
+
+  console.log(` - Rendering ${fname}...`);
+  const svg = await render(data);
+
+}
+
+
+
+
+
 async function run() {
-  console.log(await render(`
 
-machine_name: "Traffic light example";
+  validate_args();
+  const files = glob.sync(app.source)
+                    .map(fname => ({fname, data: `${fs.readFileSync(fname)}`}))
+                    .map(output);
 
-Green 'next' => Yellow 'next' => Red 'next' => Green;
-[Red Yellow Green] ~> Off -> Red;
+  await Promise.all(files);
+  console.log('... finished');
 
-  `));
 }
 
 
@@ -65,3 +145,16 @@ Green 'next' => Yellow 'next' => Red 'next' => Green;
 
 
 run();
+
+
+
+
+
+// console.log(await render(`
+
+// machine_name: "Traffic light example";
+
+// Green 'next' => Yellow 'next' => Red 'next' => Green;
+// [Red Yellow Green] ~> Off -> Red;
+
+// `));
